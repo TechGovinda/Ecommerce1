1de480bda43d68f3f213ab32b34759f762c460b8
govinda sah <sahgovinda401@gmail.com>
2025-09-27 13:29:15 +0530

    mytenthcommit

--- build_static.sh
+++ build_static.sh
@@ -1,24 +1,9 @@
-    #!/bin/bash
-    # echo "Building the project..."
-    # python3.9 -m pip install -r requirements.txt
-    # echo "Make Migration..."
-    # python3.9 manage.py makemigrations --noinput
-    # python3.9 manage.py migrate --noinput
-    # echo "Collect Static..."
-    # python3.9 manage.py collectstatic --noinput --clear
-
-
-    #!/bin/bash
+#!/bin/bash
 set -e
 
-# Install Python dependencies
-pip install --upgrade pip
-pip install -r requirements.txt
-
-# Run database migrations
-python manage.py migrate --noinput
+# Install dependencies
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
-echo "Build completed successfully."
-    
+python3 manage.py collectstatic --noinput

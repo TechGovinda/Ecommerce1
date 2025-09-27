80da244efdab15e6e6d9d348933aaf08693371e7
govinda sah <sahgovinda401@gmail.com>
2025-09-27 13:25:50 +0530

    myninthcommit

--- wsgi.py
+++ wsgi.py
@@ -8,10 +8,9 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce.settings")
 
-app = get_wsgi_application()
+app = get_wsgi_application()   # 👈 This is what Vercel expects
 
